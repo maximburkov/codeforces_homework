1698aee0cad98d282ebee6902b808f6284b53530
maximburkov <maxburkov12@gmail.com>
2020-03-11 11:40:55 +0300

    Ilya and requests done

--- Program.cs
+++ Program.cs
@@ -5,33 +5,29 @@ namespace AlgoContest
 {
     class Program
     {
-        private static int _count = 0;
-
-        static void Solve(int sum, int max)
-        {
-            if (sum == 0)
-            {
-                _count++;
-                return;
-            }
-            for (int i = max; i >= 1; i--)
-            {
-                if (i <= sum)
-                {
-                    Solve(sum - i, i);
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            var buf = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int n = buf[0];
-            int m = buf[1];
+            var s = Console.ReadLine().ToCharArray();
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[s.Length];
 
-            Solve(n, m);
+            char prev = s[0];
+            arr[0] = 1;
 
-            Console.WriteLine(_count);
+            for (int i = 1; i < s.Length; i++)
+            {
+                arr[i] = s[i] == prev ? arr[i - 1] + 1 : arr[i - 1];
+                prev = s[i];
+            }
+
+            int left, right;
+            for (int i = 0; i < n; i++)
+            {
+                var buf = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                left = buf[0] - 1;
+                right = buf[1] - 1;
+                Console.WriteLine(arr[right] - arr[left]);
+            }
         }
     }
 }
